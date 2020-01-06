cab967d0a2645971edf2a19dbfc5f7c14ba42c61
yukseltron <h.yuksel04@gmail.com>
2020-01-05 21:09:07 -0500

    added last watered calendar

--- Complete/Flora-Keeper/Plants/PlantDetail.swift
+++ Complete/Flora-Keeper/Plants/PlantDetail.swift
@@ -9,79 +9,46 @@ struct PlantDetail: View {
         userData.plants.firstIndex(where: { $0.id == plant.id })!
     }
     
+    var rkManager3 = RKManager(calendar: Calendar.current, minimumDate: Date(), maximumDate: Date().addingTimeInterval(60*60*24*365), mode: 3)
+    @State var multipleIsPresented = false
+    
     var body: some View {
         VStack {
             CircleImage(image: plant.image)
             
             Text(verbatim:
-                plant.species)
-                .font(.title)
-                .italic()
+                plant.name)
+                .font(.largeTitle)
                 .padding(.top, 20)
             
-            Button(action: {
-                self.userData.plants[self.plantIndex]
-                    .isFavorite.toggle()
-            }) {
-                if self.userData.plants[self.plantIndex]
-                    .isFavorite {
-                    Image(systemName: "house.fill")
-                        .foregroundColor(Color.green)
-                } else {
-                    Image(systemName: "house")
-                        .foregroundColor(Color.gray)
-                }
-            }
-            .padding(.bottom, 30)
+            Text(verbatim:
+                plant.species)
+                .font(.subheadline)
+                .italic()
+                .padding(.top, 10)
             
             VStack(alignment: .leading) {
                 HStack() {
-                    if (plant.soilPref.rawValue == "Moist") {
-                         Rectangle()
-                             .cornerRadius(5)
-                             .frame(width: 20, height: 20)
-                             .foregroundColor(Color(red: 80/255, green: 65/255, blue: 64/255, opacity: 1.0))
-                             .padding(.trailing, 30)
-                     } else if (plant.soilPref.rawValue == "Damp") {
-                         Rectangle()
-                             .cornerRadius(5)
-                             .frame(width: 20, height: 20)
-                             .foregroundColor(Color(red: 169/255, green: 124/255, blue: 119/255, opacity: 1.0))
-                            .padding(.trailing, 30)
-                     } else {
-                         Rectangle()
-                             .cornerRadius(5)
-                             .frame(width: 20, height: 20)
-                             .foregroundColor(Color(red: 243/255, green: 198/255, blue: 119/255, opacity: 1.0))
-                             .padding(.trailing, 30)
-                     }
-                    
-                    Text(verbatim: String(plant.soilPref.rawValue) + " soil")
+                    Text(verbatim: "Light\t")
                         .font(.title)
                     
-                    Spacer()
-                }
-                .padding()
-                
-                HStack() {
+                    
                     if (plant.lightLevel.rawValue == "Low") {
                          Rectangle()
                              .cornerRadius(20)
                              .frame(width: 20, height: 20)
                              .foregroundColor(Color(red: 255/255, green: 196/255, blue: 51/255, opacity: 1.0))
-                             .padding(.trailing, 30)
                      } else if (plant.lightLevel.rawValue == "Medium") {
                          Rectangle()
                              .cornerRadius(20)
                              .frame(width: 20, height: 20)
                              .foregroundColor(Color(red: 255/255, green: 196/255, blue: 51/255, opacity: 1.0))
-                            .padding(.trailing, 15)
+                             .padding(.trailing, 10)
                         
                         Rectangle()
                             .cornerRadius(20)
                             .frame(width: 20, height: 20)
                             .foregroundColor(Color(red: 255/255, green: 196/255, blue: 51/255, opacity: 1.0))
-                            .padding(.trailing, 15)
                         
                      } else {
                          Rectangle()
@@ -94,36 +61,109 @@ struct PlantDetail: View {
                                .cornerRadius(20)
                                .frame(width: 20, height: 20)
                                .foregroundColor(Color(red: 255/255, green: 196/255, blue: 51/255, opacity: 1.0))
-                               .padding(.leading, 5)
                                .padding(.trailing, 10)
                         
                         Rectangle()
                                .cornerRadius(20)
                                .frame(width: 20, height: 20)
                                .foregroundColor(Color(red: 255/255, green: 196/255, blue: 51/255, opacity: 1.0))
-                               .padding(.leading, 5)
                                .padding(.trailing, 10)
                      }
-                    
-                    Text(verbatim: String(plant.lightLevel.rawValue) + " Light\t")
+
+                }
+                
+                HStack() {
+                    Text(verbatim: "Water\t")
                         .font(.title)
                     
-                    Spacer()
+                    
+                    if (plant.lightLevel.rawValue == "Low") {
+                         Rectangle()
+                             .cornerRadius(20)
+                             .frame(width: 20, height: 20)
+                             .foregroundColor(Color(red: 64/255, green: 234/255, blue: 212/255, opacity: 1.0))
+                        
+                     } else if (plant.lightLevel.rawValue == "Medium") {
+                          Rectangle()
+                              .cornerRadius(20)
+                              .frame(width: 20, height: 20)
+                              .foregroundColor(Color(red: 64/255, green: 234/255, blue: 212/255, opacity: 1.0))
+                              .padding(.trailing, 10)
+                         
+                         Rectangle()
+                                .cornerRadius(20)
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(Color(red: 64/255, green: 234/255, blue: 212/255, opacity: 1.0))
+                        
+                     } else {
+                         Rectangle()
+                             .cornerRadius(20)
+                             .frame(width: 20, height: 20)
+                             .foregroundColor(Color(red: 64/255, green: 234/255, blue: 212/255, opacity: 1.0))
+                             .padding(.trailing, 10)
+                        
+                        Rectangle()
+                               .cornerRadius(20)
+                               .frame(width: 20, height: 20)
+                               .foregroundColor(Color(red: 64/255, green: 234/255, blue: 212/255, opacity: 1.0))
+                               .padding(.trailing, 10)
+                        
+                        Rectangle()
+                               .cornerRadius(20)
+                               .frame(width: 20, height: 20)
+                               .foregroundColor(Color(red: 64/255, green: 234/255, blue: 212/255, opacity: 1.0))
+                               .padding(.trailing, 10)
+                     }
+
                 }
-                .padding()
             }
             .padding()
-            .padding(.leading, 30)
             
-            Spacer()
+            
+            Button(action: { self.multipleIsPresented.toggle() }) {
+                Text("Last Watered:").foregroundColor(.blue)
+            }
+            .sheet(isPresented: self.$multipleIsPresented, content: {
+                RKViewController(isPresented: self.$multipleIsPresented, rkManager: self.rkManager3)})
+            Text(self.getTextFromDate(date: self.rkManager3.selectedDates.last))
         }
+    }
+    
+    func datesView(dates: [Date]) -> some View {
+        ScrollView (.horizontal) {
+            HStack {
+                ForEach(dates, id: \.self) { date in
+                    Text(self.getTextFromDate(date: date))
+                }
+            }
+        }.padding(.horizontal, 15)
+    }
+    
+    func startUp() {
+        
+        // example of pre-setting selected dates
+        let testOnDates = [Date().addingTimeInterval(60*60*24), Date().addingTimeInterval(60*60*24*2)]
+        rkManager3.selectedDates.append(contentsOf: testOnDates)
+        
+        // example of some foreground colors
+        rkManager3.colors.weekdayHeaderColor = Color.blue
+        rkManager3.colors.monthHeaderColor = Color.green
+        rkManager3.colors.textColor = Color.blue
+        rkManager3.colors.disabledColor = Color.red
+    }
+    
+    func getTextFromDate(date: Date!) -> String {
+        let formatter = DateFormatter()
+        formatter.locale = .current
+        formatter.dateFormat = "EEEE, MMMM d, yyyy"
+        return date == nil ? "" : formatter.string(from: date)
     }
 }
 
 struct PlantDetail_Preview: PreviewProvider {
     static var previews: some View {
         let userData = UserData()
-        return PlantDetail(plant: userData.plants[1])
+        return PlantDetail(plant: userData.plants[3])
             .environmentObject(userData)
     }
 }
